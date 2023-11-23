94bb5ce0a6762e372db189c5cc67be43ca480eeb
yeolsss <yeol8810@gmail.com>
2023-11-23 16:52:55 +0900

    firebase 오류 수정

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import App from "./App";
-<<<<<<<<< Temporary merge branch 1
-=========
-
-import { QueryClient, QueryClientProvider } from "react-query";
->>>>>>> 4bb246c1c15fd19bcd1b58e6d4189592a2576f13
 import { RootProvider } from "./context/root.context";
 import store from "./redux/store";
->>>>>>>>> Temporary merge branch 2
 
 const queryClient = new QueryClient();
 
