61506ee23ec48f1001b5b7d7231d3394806d19fb
Liabinn <liabin124@gmail.com>
2023-11-23 16:55:49 +0900

    임시저장

--- src/page/myPage/MyPage.jsx
+++ src/page/myPage/MyPage.jsx
@@ -1,15 +1,12 @@
+import MyProfile from "../../components/mypost/mypost";
 import * as St from "./myPage.style";
 
 function MyPage() {
-<<<<<<< HEAD
-  return <St.MyPageContainer>MyPage</St.MyPageContainer>;
-=======
   return (
     <St.MyPageContainer>
       <MyProfile />
     </St.MyPageContainer>
   );
->>>>>>> 0c429475da637d098040d00bed33595fc5fdd448
 }
 
 export default MyPage;
