f69b4faa3c39074a288134e5abd2563da1c9ab8c
dodoman <rjsdn1955@naver.com>
2023-11-23 16:09:08 +0900

    홈디렉터리 경로변경

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Detail from "../page/detail/Detail";
-import Home from "../page/detail/home/Home";
+import Home from "../page/home/Home";
 import Join from "../page/join/Join";
 import MyPage from "../page/myPage/MyPage";
 import Write from "../page/write/Write";
@@ -11,11 +11,11 @@ export const Router = () => (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/my-page" element={<MyPage />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/my-page/:memberId" element={<MyPage />} />
         <Route path="/write" element={<Write />} />
         <Route path="detail/:id" element={<Detail />} />
       </Route>
-      <Route path="/join" element={<Join />} />
     </Routes>
   </BrowserRouter>
 );
