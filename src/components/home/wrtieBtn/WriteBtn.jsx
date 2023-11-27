51e8560d7bab006f7ef82af4ff2090a70e3fc7b0
yeolsss <yeol8810@gmail.com>
2023-11-27 09:29:51 +0900

    로그인이 되어있지 않을때 mypage, write 접근 막기 기능 추가

--- src/components/home/wrtieBtn/WriteBtn.jsx
+++ src/components/home/wrtieBtn/WriteBtn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as St from "./writeBtn.style";
+
 function WriteBtn() {
   const navigate = useNavigate();
   return (
